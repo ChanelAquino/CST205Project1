57fb64648154b754474bf349557494348f681f8e
ChanelAquino <chanelaquino@student.hartnell.edu>
2016-02-08 14:53:00 -0800

    Changed code so that user chooses folder of images rather than individual images

--- Aquino_project1.py
+++ Aquino_project1.py
@@ -1,16 +1,18 @@
 #Names: Chanel Aquino and Marcus Dixon
-#Date Created: 5 February 2016
+#Date Due: 12 February 2016
 #Description: CST205 Project 1 Image manipulation to replace undesireable pixels in a set of otherwise similar images.
+
+#prompt user to choose a directory
+folder = pickAFolder()
 
 #create a list of pictures
 pictures = []
 
-#prompt user to pick an image file
-#append the chosen image to the pictures list
-for i in range(0, 9):
-	imageFile = pickAFile()
-	imageDisplay = makePicture(imageFile)
-	pictures.append(imageDisplay)
+#traverse through contents of folder
+#append "1.png" to "9.png" images to pictures list
+for i in range(1, 10):
+	image = makePicture(folder + str(i) + ".png")
+	pictures.append(image)
 
 
 #create an empty picture for the final image
@@ -18,10 +20,10 @@ finalImage = makeEmptyPicture(495, 557)
 
 #function to obtain median values
 def medianOdd(values):
-	listLength = len(values)
- 	sortedValues = sorted(values)
-	middleIndex = (listLength/2)
-	return sortedValues[middleIndex]
+  listLength = len(values)
+  sortedValues = sorted(values)
+  middleIndex = (listLength/2)
+  return sortedValues[middleIndex]
 
 #create lists for red, green, and blue values
 redValue = []
@@ -29,15 +31,15 @@ greenValue = []
 blueValue = []
 
 for w in range(0, 495):
-	for y in range(0, 557):
-		for p in range(0, 9):
-			#get pixel at pictures[p] pixel, with w as x-coordinate and y as y-coordinate
-			imagepixel = getPixel(pictures[p], w, y)
+  for y in range(0, 557):
+    for p in range(0, 9):
+      #get pixel at pictures[p] pixel, with w as x-coordinate and y as y-coordinate
+      imagepixel = getPixel(pictures[p], w, y)
 		
-			#append red, blue, and green pixel to respective value list
-			redValue.append(getRed(imagepixel))
-			blueValue.append(getBlue(imagepixel))
-			greenValue.append(getGreen(imagepixel))
+      #append red, blue, and green pixel to respective value list
+      redValue.append(getRed(imagepixel))
+      blueValue.append(getBlue(imagepixel))
+      greenValue.append(getGreen(imagepixel))
 		
 	#set redness, blueness, and greeness of pixel at (finalImage, w, y) with value medianOdd()
     setRed(getPixel(finalImage, w, y), medianOdd(redValue))
